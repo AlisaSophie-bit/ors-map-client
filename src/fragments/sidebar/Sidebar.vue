6ae7aa267c962b3a934f9558d18755e5888784c2
AlisaSophie-bit <73381661+AlisaSophie-bit@users.noreply.github.com>
2020-11-22 11:53:55 +0100

    Initial commit

--- src/fragments/sidebar/Sidebar.vue
+++ src/fragments/sidebar/Sidebar.vue
@@ -15,7 +15,8 @@
         <v-layout row class="sidebar-header-top" >
           <v-flex xs6 md9>
             <div class="logo-container">
-              <a :href="homeUrl"><img height="52.5" class="small ml-2" src="@/assets/img/logo@2x.png" :title="$t('global.appName')" :alt="$t('global.appName')"></a>
+              <!-- <a :href="homeUrl"><img height="52.5" class="small ml-2" src="@/assets/img/logo@2x.png" :title="$t('global.appName')" :alt="$t('global.appName')"></a> -->
+              <h1>Joggy</h1>
             </div>
           </v-flex>
           <v-spacer></v-spacer>
